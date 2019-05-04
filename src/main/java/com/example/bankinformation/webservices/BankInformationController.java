73268e3976d01f86342404707f567b73bb82f1a2
pradeepl <espritz.pradeep@gmail.com>
2019-05-05 02:08:58 +0530

    Fix bugs

--- src/main/java/com/example/bankinformation/webservices/BankInformationController.java
+++ src/main/java/com/example/bankinformation/webservices/BankInformationController.java
@@ -1,11 +1,13 @@
 package com.example.bankinformation.webservices;
 
 import com.example.bankinformation.objectmodel.BranchInfo;
+import com.example.bankinformation.objectmodel.SearchCriteria;
 import com.example.bankinformation.repository.dataaccess.BranchDAO;
 import com.example.bankinformation.repository.dataobject.BranchDO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,14 +22,14 @@ public class BankInformationController
     //nothing
   }
 
-  @GetMapping(value="/")
-  public ResponseEntity serviceInfo()
+  @GetMapping(value="/branches")
+  public ResponseEntity getBranchInfo(@RequestBody SearchCriteria criteria)
   {
-    return ResponseEntity.ok("Get information about bank and their branches given the IFSC code");
+    return ResponseEntity.ok("Get information about bank and their branches given the IFSC code. " + criteria.getBankName() + ", " + criteria.getCity() );
   }
 
-  @GetMapping(value="/bank")
-    public ResponseEntity getBankInfo(@RequestParam(value="bankName") String bankName)
+  @GetMapping(value="/branch")
+    public ResponseEntity getBranchInfo(@RequestParam(value="bankName") String bankName)
   {
     BranchInfo branchInfo = branchInfoAccessService.getBranchDetailsProvidedIfscCode(bankName);
 
