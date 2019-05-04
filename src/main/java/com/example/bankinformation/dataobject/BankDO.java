432295a7186e91a5f56aa1dce63fb3ca042e55fc
pradeepl <espritz.pradeep@gmail.com>
2019-05-05 00:13:25 +0530

    Fix bugs

---
+++ src/main/java/com/example/bankinformation/dataobject/BankDO.java
@@ -0,0 +1,47 @@
+package com.example.bankinformation.dataobject;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "BANKS")
+public class BankDO
+{
+  @Column(name = "ID")
+  @Id
+  private Long id;
+
+  @Column(name="NAME")
+  private String name;
+
+  public BankDO()
+  {
+
+  }
+
+  public BankDO(Long id, String name)
+  {
+    this.id=id;
+    this.name=name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+}
