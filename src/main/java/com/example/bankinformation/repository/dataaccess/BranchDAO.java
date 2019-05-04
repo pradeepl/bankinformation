85e7796366c460d475a80986b9e908a3e1ba054a
pradeepl <espritz.pradeep@gmail.com>
2019-05-05 02:51:54 +0530

    Fix bugs

--- src/main/java/com/example/bankinformation/repository/dataaccess/BranchDAO.java
+++ src/main/java/com/example/bankinformation/repository/dataaccess/BranchDAO.java
@@ -5,6 +5,9 @@ import com.example.bankinformation.repository.dataobject.BranchDO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class BranchDAO
 {
@@ -21,5 +24,23 @@ public class BranchDAO
     BranchInfo branchInfo = new BranchInfo(branchDO.getBank().getName(), branchDO.getBranchName(), branchDO.getIfsc(), branchDO.getAddress(), branchDO.getCity(), branchDO.getDistrict(), branchDO.getState());
     return branchInfo;
   }
+
+  public List<BranchInfo> getBranchDetailsProvidedBankNameAndCity(String bankName, String city)
+  {
+    List<BranchDO> branches = repo.getBranchDetailsGivenBankNameAndCity(bankName, city);
+    if (branches == null || branches.isEmpty())
+    {
+      return null;
+    }
+
+    List<BranchInfo> branchesInfo = new ArrayList<BranchInfo>();
+    for (BranchDO branchDO: branches) {
+      BranchInfo branchInfo = new BranchInfo(branchDO.getBank().getName(), branchDO.getBranchName(), branchDO.getIfsc(), branchDO.getAddress(), branchDO.getCity(), branchDO.getDistrict(), branchDO.getState());
+      branchesInfo.add(branchInfo);
+    }
+    return branchesInfo;
+  }
+
+
 
 }
