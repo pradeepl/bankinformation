181f3f43a35fd25fc610d69bb6c68f66ed900faf
pradeepl <espritz.pradeep@gmail.com>
2019-05-05 01:30:59 +0530

    Fix bugs

--- src/main/java/com/example/bankinformation/repository/dataaccess/BranchDAO.java
+++ src/main/java/com/example/bankinformation/repository/dataaccess/BranchDAO.java
@@ -1,5 +1,6 @@
 package com.example.bankinformation.repository.dataaccess;
 
+import com.example.bankinformation.objectmodel.BranchInfo;
 import com.example.bankinformation.repository.dataobject.BranchDO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,15 @@ public class BranchDAO
   @Autowired
   BankBranchInformationRepository repo;
 
-  public BranchDO getBranchDetailsProvidedIfscCode(String ifscCode)
+  public BranchInfo getBranchDetailsProvidedIfscCode(String ifscCode)
   {
-    return repo.getBranchDetailsGivenIFSCCode(ifscCode);
+    BranchDO branchDO = repo.getBranchDetailsGivenIFSCCode(ifscCode);
+    if (branchDO == null)
+    {
+      return null;
+    }
+    BranchInfo branchInfo = new BranchInfo(branchDO.getBank().getName(), branchDO.getBranchName(), branchDO.getIfsc(), branchDO.getAddress(), branchDO.getCity(), branchDO.getDistrict(), branchDO.getState());
+    return branchInfo;
   }
 
 }
