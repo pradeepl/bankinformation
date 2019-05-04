daa5f62ba36586789ecacc77fc2ae0fa23d1c7ee
pradeepl <espritz.pradeep@gmail.com>
2019-05-05 04:22:50 +0530

    Fix bugs

--- src/main/java/com/example/bankinformation/repository/dataaccess/BranchDAO.java
+++ src/main/java/com/example/bankinformation/repository/dataaccess/BranchDAO.java
@@ -16,11 +16,15 @@ public class BranchDAO
 
   public BranchInfo getBranchDetailsProvidedIfscCode(String ifscCode)
   {
+    System.out.println("DAO: Going to find the branch details for the ifsc: " + ifscCode);
     BranchDO branchDO = repo.getBranchDetailsGivenIFSCCode(ifscCode);
     if (branchDO == null)
     {
+      System.out.println("DAO: No branch found for the ifsc: " + ifscCode);
       return null;
     }
+
+    System.out.println("DAO: Branch " + branchDO.getBranchName() + " belonging to " + branchDO.getBank().getName() + " found for the ifsc: " + ifscCode);
     BranchInfo branchInfo = new BranchInfo(branchDO.getBank().getName(), branchDO.getBranchName(), branchDO.getIfsc(), branchDO.getAddress(), branchDO.getCity(), branchDO.getDistrict(), branchDO.getState());
     return branchInfo;
   }
@@ -30,9 +34,11 @@ public class BranchDAO
     List<BranchDO> branches = repo.getBranchDetailsGivenBankNameAndCity(bankName, city);
     if (branches == null || branches.isEmpty())
     {
+      System.out.println("DAO: No branch found for the bank: " + bankName + " in city: " + city);
       return null;
     }
 
+    System.out.println("DAO: Found " + branches.size() + " for the bank: " + bankName + " in city: " + city);
     List<BranchInfo> branchesInfo = new ArrayList<BranchInfo>();
     for (BranchDO branchDO: branches) {
       BranchInfo branchInfo = new BranchInfo(branchDO.getBank().getName(), branchDO.getBranchName(), branchDO.getIfsc(), branchDO.getAddress(), branchDO.getCity(), branchDO.getDistrict(), branchDO.getState());
