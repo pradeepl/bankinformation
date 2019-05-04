66b18ffb2e9a0bf3b00f7b88adc544abb9a422ea
pradeepl <espritz.pradeep@gmail.com>
2019-05-05 02:51:43 +0530

    Fix bugs

--- src/main/java/com/example/bankinformation/repository/dataaccess/BankBranchInformationRepository.java
+++ src/main/java/com/example/bankinformation/repository/dataaccess/BankBranchInformationRepository.java
@@ -10,7 +10,11 @@ import java.util.List;
 @Repository
 public interface BankBranchInformationRepository extends CrudRepository<BranchDO, String>
 {
-  //List<BranchDO> getBranchDetailsGivenBankNameAndCity(String bankName, String city);
+
+  @Query("SELECT br FROM BranchDO br WHERE UPPER(br.bank.name) = UPPER(?1) AND UPPER(br.city) = UPPER(?2)")
+  List<BranchDO> getBranchDetailsGivenBankNameAndCity(String bankName, String city);
+
+
   @Query("SELECT b from BranchDO b where UPPER(ifsc) = UPPER(?1)")
   BranchDO getBranchDetailsGivenIFSCCode(String ifsc);
 }
