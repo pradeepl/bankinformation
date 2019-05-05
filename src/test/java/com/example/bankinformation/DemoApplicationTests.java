59c1b80a1eeb3c00009d4cac846d9a5f31c1e312
pradeepl <espritz.pradeep@gmail.com>
2019-05-05 12:51:06 +0530

    Fix bugs

--- src/test/java/com/example/bankinformation/DemoApplicationTests.java
+++ src/test/java/com/example/bankinformation/DemoApplicationTests.java
@@ -3,14 +3,23 @@ package com.example.bankinformation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest
 public class DemoApplicationTests {
 
-	//@Test
+//	@Test
 	public void contextLoads() {
+    TestRestTemplate restTemplate = new TestRestTemplate();
+    ResponseEntity<String> response = restTemplate.getForEntity("https://indian-bank-information.herokuapp.com/branch_info?bank_name=ABHYUDAYA COOPERATIVE BANK LIMITED&city=MUMBAI", String.class);
+    assertThat(response.getStatusCode().equals(HttpStatus.OK.value()));
 	}
 
 }
